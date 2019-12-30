9cc2878766cf55f1fd57be5002cb42860ce0bc11
邓舟淞 <1184566576@qq.com>
2019-12-30 15:29:27 +0800

    第一次提交

---
+++ src/pages/demo/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view class="list">
+  <view  @click="goDetail(item.id)"  v-for="(item,index) in heroList" :key = "index">
+    <image :src="item.iconUrl"></image>
+    <text class="name">{{item.name}}</text>
+  </view>
+</view>
+</template>
+
+<script>
+import heroList from '../../data/lol_duowan'
+export default {
+  data () {
+    return {
+      heroList
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
